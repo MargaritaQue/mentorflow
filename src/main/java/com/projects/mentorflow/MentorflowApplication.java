dc5198681d99fded6b37f0d09dff790f31a20e4a
Маргарита <margarita@MacBook-Air-Margarita.local>
2026-09-22 06:38:45 +0300

    Initial commit

---
+++ src/main/java/com/projects/mentorflow/MentorflowApplication.java
@@ -0,0 +1,13 @@
+package com.projects.mentorflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/mentorflow/MentorflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.mentorflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
